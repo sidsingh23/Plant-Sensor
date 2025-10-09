e64d5586a76ff169c54d36e7eef342ff6110438d
Siddharth Singh <tosiddharthsingh@gmail.com>
2025-10-09 13:39:05 -0500

    edited decimal conversion function

--- lambda_function.py
+++ lambda_function.py
@@ -6,11 +6,11 @@ import json
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table("PlantSensorData")
 
-def decimal_to_native(obj):
+def decimalConvert(obj):
     if isinstance(obj, list):
-        return [decimal_to_native(i) for i in obj]
+        return [decimalConvert(i) for i in obj]
     elif isinstance(obj, dict):
-        return {k: decimal_to_native(v) for k, v in obj.items()}
+        return {k: decimalConvert(v) for k, v in obj.items()}
     elif isinstance(obj, Decimal):
         return int(obj) if obj % 1 == 0 else float(obj) #int or float choice happens here
     else:
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
             'body': json.dumps({"message": "No data found for this sensor"})
         }
     
-    item = decimal_to_native(response['Items'][0])
+    item = decimalConvert(response['Items'][0])
     
     return {
         'statusCode': 200,
